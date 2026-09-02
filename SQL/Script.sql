b068536de465c9514e266fa8a409b4bd19cb329d
Guilherme de Oliveira <guioliveira95@gmail.com>
2026-09-02 19:12:32 -0300

    continuacao de script sql

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -1,3 +1,4 @@
+-- Criação da tabela: Frutas
 CREATE TABLE Fruta (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
@@ -11,11 +12,8 @@ VALUES ('Maçã', 'Vermelha', 5.50);
 SELECT id, nome, cor, preco
 FROM abcdef_aula.Fruta;
 
-
-
--- TABELA: clientes
-
-CREATE TABLE clientes (
+-- Criação da tabela: Clientes
+CREATE TABLE Clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
@@ -26,9 +24,8 @@ CREATE TABLE clientes (
     estado CHAR(2)
 );
 
--- TABELA: produtos
-
-CREATE TABLE produtos (
+-- Criação da tabela: Produtos
+CREATE TABLE Produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
     descricao TEXT,
@@ -37,28 +34,35 @@ CREATE TABLE produtos (
     ativo BOOLEAN NOT NULL DEFAULT TRUE
 );
 
--- TABELA: pedidos
-
-CREATE TABLE pedidos (
+-- Criação da tabela: Pedidos
+CREATE TABLE Pedidos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT NOT NULL,
     id_produto INT NOT NULL,
+    id_estado INT NOT NULL,
     quantidade INT NOT NULL DEFAULT 1,
     data_pedido DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_pedido_cliente
         FOREIGN KEY (id_cliente)
-        REFERENCES clientes(id),
+        REFERENCES Clientes(id),
 
     CONSTRAINT fk_pedido_produto
         FOREIGN KEY (id_produto)
-        REFERENCES produtos(id)
+        REFERENCES Produtos(id),
+        
+	CONSTRAINT fk_pedido_estado
+		FOREIGN KEY (id_estado)
+		REFERENCES Estado(id)
 );
 
-CREATE TABLE estado (
+-- Criação da tabela: Estado
+CREATE TABLE Estado (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     sigla VARCHAR(2) NOT NULL
 );
 
-
+INSERT INTO abcdef_aula.Cliente
+(nome, email, telefone, cpf, endereco, cidade, estado)
+VALUES('João Silva', 'joao@gmail.com', '5199986554', '12345678910', 'Rua Marechal Deodoro, 99', 'Santa Cruz do Sul', 'RS');
