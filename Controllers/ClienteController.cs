362ecfcec30b06443beb15b6ccbbde4c5b04c729
Guilherme de Oliveira <guioliveira95@gmail.com>
2026-09-02 22:00:54 -0300

    Criação de DTOs e utilização

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 using AulaBackend_API.Data;
+using AulaBackend_API.DTOs;
 using AulaBackend_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,39 @@ namespace AulaBackend_API.Controllers
             var clientes = await _context.Cliente.ToListAsync();
 
             return Ok(clientes);
+        }
+
+        // POST: api/Cliente
+        // Cadastrar cliente
+        [HttpPost]
+        public async Task<ActionResult<Cliente>> PostProduto(ClienteRequest request)
+        {
+            // Remove tudo que não for número
+            string cpfLimpo = new string(
+                request.Cpf.Where(char.IsDigit).ToArray()
+            );
+            // Converte para inteiro
+            int cpf = int.Parse(cpfLimpo);
+            var cliente = new Cliente
+            {
+                Nome = request.Nome,
+                Email = request.Email,
+                Telefone = request.Telefone,
+                Cpf = cpf,
+                Endereco = request.Endereco,
+                Cidade = request.Cidade,
+                Estado = request.Estado,
+            };
+
+            _context.Produto.Add(cliente);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetProduto),
+                new { id = produto.Id },
+                produto
+            );
         }
     }
 }
